d06ccc24f45ff13e446e1b52bd49651f2673a981
mikiklimko <mikimiki.klimko@gmail.com>
2021-04-12 17:22:13 +0200

    commit

--- app.js
+++ app.js
@@ -5,12 +5,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
 
 
@@ -21,12 +15,10 @@ require('./config/passport')(passport);
 
 // Databaza config
 const db = require('./config/keys').MongoURI;
-const { resolve } = require('path');
-const { rejects } = require('assert');
 
 //pripojenie na mongo
  
-const conn = mongoose.connect  (db, {  useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect  (db, {  useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Databaza pripojena'))
     .catch(err => console.log(err));
 
@@ -39,70 +31,6 @@ app.set('view engine', 'ejs');
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
-
-/* 
-let gfs;
-console.log(conn);
- conn.then('open', () => {
-    // Init stream
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-  });
-
-  const storage = new GridFsStorage({
-    url: db,
-    options: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    },
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString('hex') + path.extname(file.originalname);
-          const fileInfo = {
-            filename: filename,
-            bucketName: 'uploads'
-          };
-          resolve(fileInfo);
-        });
-      });
-    }
-  });
-  const upload = multer({ storage });
-
-//Route get /uploads
-// Loads form
-app.get('/uploads', (req,res) =>{
-  res.render('uploads');
-})
- 
-//route post /upload
-// Uploads file to db 
-//to 'file' je z form v name=file
-app.post('/upload', upload.single("file"), (req,res) =>{
-  // res.json({file: req.file}); 
-   res.redirect('/uploads')
- });
-
- //route gett /files
-// display all files in json
-
-app.get('/files', (req, res) => {
-  gfs.files.find({}).toArray((err, files) => {
-      //check if files exist
-      if (!files || files.length == 0) {
-          return res.status(404).json({
-              err: "No files exist"
-          })
-      }
-      // files exist
-      return res.json(files)
-  })
-}) */
 
 //Express session
 app.use(session({
@@ -130,7 +58,6 @@ app.use((req, res, next) => {
 //routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
-/* app.use('/', require('./routes/upload.js'));    */
 
 
 const PORT = process.env.PORT || 5000;
